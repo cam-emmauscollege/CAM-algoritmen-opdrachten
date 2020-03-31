fe6271ae8e494f2cb8ce4015529b31ce5a62ba3e
cam-emmauscollege <45658387+cam-emmauscollege@users.noreply.github.com>
2020-03-31 14:45:56 +0200

    added JSDoc comments

--- opdracht-1.js
+++ opdracht-1.js
@@ -1,10 +1,12 @@
-/* Algoritmiek opgave #1
-   Opdracht: pas de functie zo aan,
-   dat de functie true geeft als 'getal'
-   0 of positief is en false als 'getal'
-   negatief is.
-*/
-
+/** Algoritmiek opgave #1
+  * Opdracht: pas de functie zo aan,
+  * dat de functie true geeft als 'getal'
+  * 0 of positief is en false als 'getal'
+  * negatief is.
+  *
+  * @param getal {number} een getal
+  * @returns {boolean} true als getal positief is
+  */
 function isPositief(getal) {
   var teruggeefWaarde = false;
 
